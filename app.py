da29b884bbf02ae3f4a2e58f456fc739ee4a539c
Akanksha <akankshasingh47804@gmail.com>
2024-11-15 20:34:13 +0530

    new

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras.models import load_model
 import numpy as np
 import pickle
+from PIL import Image
+import json
 
 
 app = Flask(__name__, template_folder='templates')
@@ -18,9 +21,55 @@ crop_recommendation_model = pickle.load(open(crop_recommendation_model_path, 'rb
 
 # deficiency_prediction_model_path = 'models/Deficiency_prediction.pkl'
 # deficiency_prediction_model = pickle.load(open(deficiency_prediction_model_path, 'rb'))
+model = load_model('models/plant_disease_prediction_model.h5')
 
-# plant_disease_model_path = 'models\plant-disease-model.pth'
-# plant_disease_model == pickle.load(open(plant_disease_model_path, 'rb'))
+# # with open('models/class_indices.json', 'r') as f:
+# #     class_indices = json.load(f)
+
+# class_names = {v: k for k, v in class_indices.items()}
+
+class_names={0: 'Apple___Apple_scab',
+ 1: 'Apple___Black_rot',
+ 2: 'Apple___Cedar_apple_rust',
+ 3: 'Apple___healthy',
+ 4: 'Blueberry___healthy',
+ 5: 'Cherry_(including_sour)___Powdery_mildew',
+ 6: 'Cherry_(including_sour)___healthy',
+ 7: 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+ 8: 'Corn_(maize)___Common_rust_',
+ 9: 'Corn_(maize)___Northern_Leaf_Blight',
+ 10: 'Corn_(maize)___healthy',
+ 11: 'Grape___Black_rot',
+ 12: 'Grape___Esca_(Black_Measles)',
+ 13: 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+ 14: 'Grape___healthy',
+ 15: 'Orange___Haunglongbing_(Citrus_greening)',
+ 16: 'Peach___Bacterial_spot',
+ 17: 'Peach___healthy',
+ 18: 'Pepper,_bell___Bacterial_spot',
+ 19: 'Pepper,_bell___healthy',
+ 20: 'Potato___Early_blight',
+ 21: 'Potato___Late_blight',
+ 22: 'Potato___healthy',
+ 23: 'Raspberry___healthy',
+ 24: 'Soybean___healthy',
+ 25: 'Squash___Powdery_mildew',
+ 26: 'Strawberry___Leaf_scorch',
+ 27: 'Strawberry___healthy',
+ 28: 'Tomato___Bacterial_spot',
+ 29: 'Tomato___Early_blight',
+ 30: 'Tomato___Late_blight',
+ 31: 'Tomato___Leaf_Mold',
+ 32: 'Tomato___Septoria_leaf_spot',
+ 33: 'Tomato___Spider_mites Two-spotted_spider_mite',
+ 34: 'Tomato___Target_Spot',
+ 35: 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+ 36: 'Tomato___Tomato_mosaic_virus',
+ 37: 'Tomato___healthy'}
+
+
+
+
 
 @app.route('/')
 def index():
@@ -82,6 +131,11 @@ def logout():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/how-it-works')
+def how_it_works():
+    return render_template("how_it_works.html")
+
 
 @app.route('/services')
 def services():
@@ -167,5 +221,37 @@ def crop_prediction():
 @app.route('/disease')
 def disease():
     return render_template('disease.html')
+
+
+def load_and_preprocess_image(image, target_size=(224, 224)):
+    img = Image.open(image)
+    img = img.convert("RGB")
+    img = img.resize(target_size)
+    img_array = np.array(img)
+    img_array = np.expand_dims(img_array, axis=0) / 255.0
+    return img_array
+
+  
+ 
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return "No file uploaded", 400
+    
+    file = request.files['file']
+    if file:
+        # Preprocess the uploaded image
+        img_array = load_and_preprocess_image(file)
+        
+        # Make prediction
+        predictions = model.predict(img_array)
+        predicted_class_index = np.argmax(predictions, axis=1)[0]
+        predicted_class_name = class_names[predicted_class_index]
+        
+        # Pass prediction to disease_result.html
+        return render_template('disease_result.html', prediction=predicted_class_name)
+    else:
+        return redirect(url_for('disease'))
+    
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
